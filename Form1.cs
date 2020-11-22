e05e3d12c7b6ba2199aafc7c8f531888de257438
俣喆 施 <Dylan_233@foxmail.com>
2020-11-22 09:24:25 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slay_the_spire_text_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
